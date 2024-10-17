b5fd2f58c8e44c8b901abd2c42d59173ad5d3c41
Ruslan-Jdanov <ruslan_jdanov@student.itpu.uz>
2024-10-17 12:54:34 +0500

    Updated app.py to include full greeting functionality

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 print("I'm learning Git merge conflicts")
 
 name = input('Please enter your name:')
-last name = input('Please enter your last name:')
+last_name = input('Please enter your last name:')
 nick = input('Please enter your nickname')
-print('Hi, {} {}!'.format(name, last name))
+print('Hi, {} {}!'.format(name, last_name, nick))
